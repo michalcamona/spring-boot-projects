2c3583fd131afd09dd8d799a98266ef73565dd3e
michalcamona <michal.camona@gmail.com>
2016-12-11 19:09:55 +0100

    chg

---
+++ spring-cloud-task/src/main/java/pl/mica/spring/cloud/SpringCloudTaskApplication.java
@@ -0,0 +1,46 @@
+package pl.mica.spring.cloud;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableTask
+public class SpringCloudTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudTaskApplication.class, args);
+	}
+	
+	@Bean
+	public TollProcessingTask tollProcessingTask() {
+		return new TollProcessingTask();
+	}
+
+	public class TollProcessingTask implements CommandLineRunner {
+
+		@Override
+		public void run(String... strings) throws Exception {
+
+			//parameters: stationid, license plate, timestam
+			
+			if(null != strings){
+				System.out.println("parameter length: " + strings.length);
+				
+				if(strings.length > 0){
+					String stationId = strings[1];
+					String licensePlate = strings[2];
+					String timestamp = strings[3];
+					
+					System.out.println("Station ID is " + stationId + ", plate is " + licensePlate + ", timestamp is " + timestamp);
+				}
+			}
+			
+			System.out.println("Task completed.");
+
+		}
+
+	}
+}
